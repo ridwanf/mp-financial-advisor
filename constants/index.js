2b2293763513519b46eb9a5d1a74f9962cca54d5
pratamari <158981968+pratamari@users.noreply.github.com>
2025-05-23 13:13:04 +0700

    Delete constants/index.js

--- constants/index.js
+++
@@ -1,3 +0,0 @@
-export const HERMES_KEY =  'app-nIXyL9FQmN7lC99AcspXH2aJ'
-export const HERMES_URL = 'https://hermes-flow.platform.danaventures.id/v1/workflows/run'
-export const HERMES_USER = 'ridwanf'
