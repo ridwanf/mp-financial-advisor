f033cb1027ffa1ea1f8067fa6de5acfac3b07749
Rikard Agus Pratama <rikard.pratama@dana.id>
2025-05-23 13:15:33 +0700

    revert index

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,3 @@
-export const HERMES_KEY =  'app-nIXyL9FQmN7lC99AcspXH2aJ'
-export const HERMES_URL = 'https://hermes-flow.platform.danaventures.id/v1/workflows/run'
-export const HERMES_USER = 'ridwanf'
+export const HERMES_KEY = ''
+export const HERMES_URL = ''
+export const HERMES_USER = ''
